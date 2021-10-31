a6371b2f8a1a64d9f6181be76084e962a6778a7b
Alexandru Filipescu <alex98.madrid@yahoo.es>
2021-10-31 10:59:55 +0100

    Just modified App.js

--- App.js
+++ App.js
@@ -26,14 +26,16 @@ function filterIt(searchTags)
 
 }
 
-const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+// const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
 
-function isPrime(number)
-{ 
-    return number >10;
-}
+// function isPrime(number)
+// { 
+//     return number >10;
+// }
 
-console.log(array.filter(isPrime));
+// console.log(array.filter(isPrime));
+
+
 
 server.listen(port, hostname, () => 
 {
@@ -41,9 +43,9 @@ server.listen(port, hostname, () =>
 
     //lib.startParsing('scg.bib');  //This is the .bib => JSON
     
-    let lookedFor = ['Oscar'];
+    let lookedFor = ['Nierstrasz'];
 
-    //console.log(filterIt(lookedFor));
+    console.log(filterIt(lookedFor));
 
   
 })
